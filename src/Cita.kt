0ae187c27d514766801788e5cb3d9be3b3925150
DarwinMGQ98 <161479311+DarwinMGQ98@users.noreply.github.com>
2024-12-08 10:25:50 -0500

    Delete src directory

--- src/Cita.kt
+++
@@ -1,8 +0,0 @@
-data class Cita(
-    val id: Int,
-    var fecha: String,
-    var motivo: String,
-    var duracion: Double,
-    var costo: Double,
-    var realizada: Boolean
-)
--- src/Veterinaria.kt
+++
@@ -1,7 +0,0 @@
-data class Veterinaria(
-    val id: Int,
-    var nombre: String,
-    var direccion: String,
-    var telefono: String,
-    val citas: MutableList<Cita> = mutableListOf()
-)
